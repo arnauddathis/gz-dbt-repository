21b57cf2d806085133c3e964b11fc60707236beb
arnaud.dathis@hec.edu <arnaud.dathis@hec.edu>
2024-02-07 10:20:00 +0000

    Right Folders and materialization

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,32 +1,12 @@
-WITH join_sub AS (
-
 SELECT 
-sales.date_date,
-sales.orders_id,
-sales.products_id,
-sales.revenue,
-sales.quantity, 
-CAST (products.purchase_price AS FLOAT64) AS purchase_price
-
-FROM {{ ref('stg_raw__sales') }} AS sales
-
-LEFT JOIN {{ ref('stg_raw__product') }} AS products
-
-USING (products_id))
-
-
-SELECT 
-
-date_date,
-orders_id,
-products_id,
-revenue,
-quantity,
-purchase_price,
-quantity*purchase_price AS purchase_cost,
-ROUND(revenue-(quantity*purchase_price),2) AS margin
-
-FROM join_sub
-
-
-
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
