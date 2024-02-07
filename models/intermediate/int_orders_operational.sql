955739499ec1f903a5a65302ee9506a108f1d274
arnaud.dathis@hec.edu <arnaud.dathis@hec.edu>
2024-02-07 09:51:32 +0000

    clean int order operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,30 +1,13 @@
-WITH sub_ope AS (
 
 SELECT *, 
-ship.logcost AS logicost, 
-ship.shipping_fee AS shipping_fi, 
-ship.ship_cost AS ship_kost
+ROUND(((margin + shipping_fee - ship_cost -logcost)), 2) AS operational_margin,
+ship.logcost,
+ship.shipping_fee, 
+ship.ship_cost
 
  FROM {{ ref('int_orders_margin') }} AS orders_margin
 
 RIGHT JOIN {{ ref("stg_raw__ship") }} AS ship
-USING (orders_id)) 
+USING (orders_id)
 
-SELECT 
-
-orders_id, 
-date_date, 
-ROUND(SUM((margin + shipping_fi - ship_kost -logicost)), 2) AS operational_margin, 
-ROUND(SUM(revenue),2) AS revenue, 
-ROUND(SUM(margin), 2) AS margin,
-ROUND(SUM(quantity),2) AS quantity
-FROM sub_ope
-
-GROUP BY orders_id, date_date
-
-
-
-
-
-
-
+ORDER BY orders_id desc
